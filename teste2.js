2a7ad5185508afe52d27bf98b405edb05827939c
Cecilia <ceciliaperet81@gmail.com>
2024-03-17 20:07:23 -0300

    teste pra estagio

--- teste2.js
+++ teste2.js
@@ -2,7 +2,6 @@
 // o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando 
 //se o número informado pertence ou não a sequência.
 
-// Path: teste3.js
 
 function fibo(num){
 
